491c35aa1a80d9bff330512ca3c454166bc25467
bubbleteabish <sierra.cotnoir@gmail.com>
2025-11-05 12:08:14 -0600

    Added boba bullet

---
+++ scripts/boba_bullet.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+@export var speed := -300
+
+func _process(delta: float) -> void:
+	position.y += speed * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -11,6 +11,8 @@ var hit_ground = false
 var plat_hit
 var plat_name 
 
+var can_shoot = true
+var boba_bullet = preload("res://scenes/boba_bullet.tscn")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -46,8 +48,13 @@ func _physics_process(delta: float) -> void:
 		var ro_delta = randf_range(.1, .5)
 		rotation_speed = move_toward(rotation_speed, 0, ro_delta)
 		$Body.rotation += rotation_speed * last_dir * delta
-	if fire > 0:
-		print("FIRE")
+	if fire > 0 and can_shoot:
+		var bullet = boba_bullet.instantiate()
+		bullet.global_position.y = self.global_position.y - ($Body.texture.get_height()/2)
+		bullet.global_position.x = self.global_position.x
+		get_parent().add_child(bullet)
+		can_shoot = false
+		$Timer.start(1.5)
 	move_and_slide()
 
 
@@ -55,3 +62,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	plat_name = body.get_child(0).name
 	plat_hit = body
 	
+
+func _on_timer_timeout() -> void:
+	can_shoot = true
